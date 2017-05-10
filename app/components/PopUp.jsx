7cd01c921e6129171e0e86238e41fc68006cec2e
Claire Freehafer <claire.freehafer@gmail.com>
2017-05-09 20:30:32 -0400

    pop up alignment fixed

--- app/components/PopUp.jsx
+++ app/components/PopUp.jsx
@@ -4,21 +4,42 @@ import Dialog from 'material-ui/Dialog';
 import FlatButton from 'material-ui/FlatButton';
 
 const PopUp = (props) => {
+	var dialogStyles = {
+    position: 'absolute',
+    left: '50%',
+    top: '50%',
+    transform: 'translate(-50%, -50%)'
+};
+
 	const closeButton = <FlatButton
 		label="Close"
 		primary={true}
 		onClick={() => props.handlePopUpClose()}
 	/>
 
+	const info = props.restaurantInfo
+
+	const lat = info.geometry.location.lat();
+	const lng = info.geometry.location.lng();
+
+	const mapUrl = `https://maps.googleapis.com/maps/api/staticmap?key=AIzaSyDDRqrlHYalYAQtC_fPwZ9Z9JWAKDgD6MM&markers=${lat},${lng}&zoom=15&size=400x400`
+
 	return (
 		<div>
 		<Dialog
-			title="test"
+			title={info.name}
 			open={true}
 			actions={closeButton}
+			autoScrollBodyContent={true}
+			contentStyle={ dialogStyles }
 			onRequestClose={() => props.handlePopUpClose()}
 		>
-			test
+			<img src={mapUrl} />
+			<b>Open Now?</b> {info.opening_hours.open_now ? <span>Yes</span> : <span>No</span>}<br />
+
+			<b>Address:</b><br />gm
+
+			{info.formatted_address}
 		</Dialog>
 		</div>
 	)
